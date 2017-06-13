09cfec33088247215e4d509117d77ad79a1a1258
Solange <sol.decarvalho@hotmail.com>
2017-06-12 21:22:58 -0300

    “Criação do projeto.”

---
+++ meusistema/src/main/java/br/univel/Aluno.java
@@ -0,0 +1,36 @@
+package br.univel;
+
+public class Aluno {
+
+	private long id;
+	private String nome;
+	private String telefone;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	
+	//C:\Users\Solange\git\Ava160445
+	//https://github.com/solangecarvalho/Ava160445.git
+
+}
